af0c2e7a4ae65cac0061116e21460a1431a4f7b2
wudyn1 <1275226045@qq.com>
2026-05-16 07:58:55 +0800

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+var CACHE_NAME = 'wall-hole-calc-v4';
+var URLS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', function(e) { e.waitUntil(caches.open(CACHE_NAME).then(function(c) { return c.addAll(URLS); })); });
+self.addEventListener('fetch', function(e) { e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); })); });
+self.addEventListener('activate', function(e) { e.waitUntil(caches.keys().then(function(ks) { return Promise.all(ks.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); })); })); });
